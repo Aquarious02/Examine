f10f8fabb8d0f259d8507b715cf83aee5e4289f0
Aquarious <45901188+Aquarious02@users.noreply.github.com>
2023-04-04 23:57:15 +0300

    operating time

--- pygen/device.py
+++ pygen/device.py
@@ -24,16 +24,11 @@ class Device(Caller):
         version = 0x251d1696c
         serial = 0x2457c7116
         current_time = 0x6b13fac9
+        operating_time = 0x216067f17
 
     class ActiveBus(IntEnum):
         main = 0
         reserve = 1
-
-    @dataclass
-    class OperatingTimeInfo:
-        reboot_count: int
-        operating_time: float
-        total_time: float
 
     @dataclass
     class Version:
