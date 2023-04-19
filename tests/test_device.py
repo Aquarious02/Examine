77c0f1af015134cdd7949bc3fa452b743af9020f
Aquarious02 <aquarious0696@gmail.com>
2023-04-19 12:29:47 +0300

    basic example

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,1 +1,18 @@
 """Проверки прибора."""
+import pytest
+from loguru import logger
+
+from pygen.device import Device
+
+
+@pytest.fixture()
+def device():
+    device_ = Device(0x12)
+
+    yield device_
+
+
+def test_version(device):
+    version = device.get_tm(device.TmId.version)
+    logger.info(f'Actual version: {version}')
+    assert version.minor > 0
